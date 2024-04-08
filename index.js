8f69d73532bd0ea1d9d4b7de20fad7bba824251f
Alphy <alphygeorge2024@cs.sjcetpalai.ac.in>
2024-04-08 19:49:05 +0530

    First Commit authetication

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookiePaser = require("cookie-parser");
+
+const userRoute = require("./routes/user");
+
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/task2',{
+    
+},).then(()=>console.log('connected sucessfully'))
+.catch((err) => {console.error(err);})
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+
+
+app.get("/", async (req, res) => {
+  
+  res.render("home", {
+    user: req.user,
+    
+  });
+});
+
+app.use("/user", userRoute);
+
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
